419d3e7966adc31a553bc2fc379778fb0bbdd54a
DiegoVictor <diegovictorgonzaga@gmail.com>
2021-07-03 00:05:42 -0300

    Export component

--- src/index.js
+++ src/index.js
@@ -1,5 +1,3 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
+import ProfileSummary from './ProfileSummary';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+export { ProfileSummary };
